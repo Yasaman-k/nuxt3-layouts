c6ac5a2d290956ff443ab4e88fdd82b894eac138
cybercoder-naj <cybercoder.nishant@gmail.com>
2022-05-05 21:20:34 +0530

    Named layouts with named slots

---
+++ layouts/sidebar.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container-fluid" style="max-width: 1200px;">
+    <div class="row">
+      <div class="col-12 col-md-6 bg-light py-3 border border-secondary">
+        <slot name="list" />
+      </div>
+      <div class="col-12 col-md-6">
+        <slot name="content" />
+      </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,21 @@
 <template>
-  <NuxtLayout name="border">
-    <h2>This is the <span>pages/index.vue</span> file.</h2>
+  <NuxtLayout name="sidebar">
+    <template #list>
+      <!--List content-->
+      <ol>
+        <li>Topic 1</li>
+        <li>Topic 2
+          <ol type="a">
+            <li>Subtopic 2.1</li>
+            <li>Subtopic 2.2</li>
+          </ol>
+        </li>
+        <li>Topic 3</li>
+      </ol>
+    </template>
+    <template #content>
+      This is the main content of <span>pages/index.vue</span> with the custom side bar.
+    </template>
   </NuxtLayout>
 </template>
 
